5967468ae75be3661e647b93e131da8e8c5dc717
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-03-30 16:33:17 +0530

    Adding nuget and connection string

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF_project.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
