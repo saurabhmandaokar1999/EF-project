774ac2caec71249f4b5f3a4cf88afe3b813cd462
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-04-01 14:29:05 +0530

    Tried diffrent linq expression.

---
+++ Controllers/CurrencyController.cs
@@ -0,0 +1,54 @@
+﻿using EF_project.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace EF_project.Controllers
+{
+    [Route("api/currencies")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CurrencyController(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+        }
+
+        [HttpGet("/getAllCurrencies")]
+        public async Task<IActionResult> GetAllCurrencies()
+        {
+            var result = await _appDbContext.Currency.ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetCurrencyAsync([FromRoute] int id)
+        {
+            var result = await _appDbContext.Currency.FindAsync(id);
+            return Ok(result);
+        }
+
+        [HttpGet("byTitle/{title}")]
+        public async Task<IActionResult> GetCurrencyByNameAsync([FromRoute] string title)
+        {
+            var result = await _appDbContext.Currency.FirstOrDefaultAsync(n => n.Title == title);
+            return Ok(result);
+        }
+        [HttpGet("{title}")]
+        public async Task<IActionResult> GetCurrencyByNameAsync([FromRoute] string title, [FromQuery] string? description)
+        {
+            var result = await _appDbContext.Currency.FirstOrDefaultAsync(n => n.Title == title
+            && ((description.IsNullOrEmpty())|| n.Description==description));
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> GetAllCurrencyAsync([FromBody] List<int> ids)
+        {
+            var result = await _appDbContext.Currency.Where(x=>ids.Contains(x.Id)).ToListAsync();
+            return Ok(result);
+        }
+    }
+}
---
+++ Controllers/LanguageController.cs
@@ -0,0 +1,25 @@
+﻿using EF_project.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_project.Controllers
+{
+    [Route("api/language")]
+    [ApiController]
+    public class LanguageController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public LanguageController(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+        }
+
+        [HttpGet("/getAllLanguage")]
+        public async Task<IActionResult> GetAllLaguages() {
+            var result =await _appDbContext.Language.ToListAsync();
+            return Ok(result);
+        }
+    }
+}
